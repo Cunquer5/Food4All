a2f3a094fed0f9a84d5e0158479599b8acfb9cec
Aakash Khangar <aakashsingh.k@somaiya.edu>
2025-03-26 00:07:48 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,40 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "@/contexts/AuthContext";
+import Index from "./pages/Index";
+import Dashboard from "./pages/Dashboard";
+import UserTypeSelection from "./pages/UserTypeSelection";
+import NotFound from "./pages/NotFound";
+import React from "react";
+
+// Create a client
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <AuthProvider>
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/select-type" element={<UserTypeSelection />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </AuthProvider>
+          </BrowserRouter>
+        </TooltipProvider>
+      </QueryClientProvider>
+    </React.StrictMode>
+  );
+};
+
+export default App;
